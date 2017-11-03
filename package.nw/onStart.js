bbb23653630ff635055aed64d7fdc5cbe3097fc8
syntax-error-undefined <33355488+syntax-error-undefined@users.noreply.github.com>
2017-11-03 14:04:58 -0500

    added messages

--- package.nw/onStart.js
+++ package.nw/onStart.js
@@ -6,9 +6,9 @@
 
 
 function onRun(){
-     
-     
-     
-     
+     throwEvent("Exe Started! NodeJS initating!);
+     throwStorage("Created and loaded!");    
+     throwNotice("Check out our GitHub at: https://github.com/syntax-error-undefined/DragonEngine");
+     throwEvent("Loaded!");
 }
 
