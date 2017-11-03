66d149e1014314cc888590f19a410608337685ea
syntax-error-undefined <33355488+syntax-error-undefined@users.noreply.github.com>
2017-11-03 14:05:09 -0500

    Update onStart.js

--- package.nw/onStart.js
+++ package.nw/onStart.js
@@ -6,7 +6,7 @@
 
 
 function onRun(){
-     throwEvent("Exe Started! NodeJS initating!);
+     throwEvent("Exe Started! NodeJS initating!");
      throwStorage("Created and loaded!");    
      throwNotice("Check out our GitHub at: https://github.com/syntax-error-undefined/DragonEngine");
      throwEvent("Loaded!");
