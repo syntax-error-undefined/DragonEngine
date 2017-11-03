190009bb8a6f3be0fe7effc2d15cac3b698d9565
syntax-error-undefined <33355488+syntax-error-undefined@users.noreply.github.com>
2017-11-03 13:57:03 -0500

    added track_1.js

---
+++ package.nw/assets/sound/track_1.js
@@ -0,0 +1,5 @@
+Game.Music.track1 = new Game.Sound.SoundElement("sound/music/dragondungeon_.mp3");
+Game.Music.track1.Loop(true);
+Game.Music.track1.description = {};
+Game.Music.track1.description.isAmbient = false;
+Game.Music.track1.description.isLooped = true; //Make sure this is set to true
